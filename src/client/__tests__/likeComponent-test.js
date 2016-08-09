3d881e0d7dfea5664ab074f81f7a51724dc26e81
Jessie Leung <jleung@thoughtworks.com>
2016-08-09 12:23:09 +1000

    Unit tests using Jest and Skindeep

--- src/client/__tests__/likeComponent-test.js
+++ src/client/__tests__/likeComponent-test.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
+let sd = require('skin-deep');
+let Like = require('../app/components/likeComponent.jsx');
 
 jest.unmock('../app/components/likeComponent.jsx');
 
-
-describe('Test component', () => {
-  var Like;
+describe('Like Component', () => {
+  let tree;
 
   beforeEach(() => {
-      Like = require('../app/components/likeComponent.jsx');
+    tree = sd.shallowRender(<Like />);
   });
 
-  it('should exist', () => {
-      var like = TestUtils.renderIntoDocument(<Like />);
+  describe('Rendering', () => {
+    it('should render the number of likes and a button', () => {
+      let likeButton = tree.subTree('button');
+      let likeNumberText = tree.subTree('p');
+      expect(likeButton.text()).toEqual('Like');
+      expect(likeNumberText.text()).toEqual('Likes: 0');
+    });
+  });
 
-      expect(TestUtils.isCompositeComponent(like)).toBeTruthy();
+  describe('OnLike', () => {
+    it('should update number of likes onLike', () => {
+      const instance = tree.getMountedInstance();
+      instance.onLike();
+      expect(instance.state).toEqual({likesCount: 1});
+    });
+  });
+
+  describe('Event handlers', () => {
+    it('should increase number of likes onClick', () => {
+      let likeButton = tree.subTree('button');
+      likeButton.props.onClick({ preventDefault() {} });
+      expect(tree.getMountedInstance().state).toEqual({likesCount: 1});
+    });
   });
 });
